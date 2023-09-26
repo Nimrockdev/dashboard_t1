e0ea4c3a6522b76dd36695d0b16121672902ba2f
Nimrockdev <jasago2010@gmail.com>
2023-09-26 14:58:08 +0200

    Integramos primer componente a modo de test

--- src/DashboardApp.jsx
+++ src/DashboardApp.jsx
@@ -1,3 +1,73 @@
-export const DashboardApp = () => {
-  return <div>DashboardApp</div>;
-};
+import TableApp from "./components/Table";
+import {
+  Grid,
+  Col,
+  Card,
+  Text,
+  Title,
+  Metric,
+  TabGroup,
+  TabPanel,
+  TabList,
+  Tab,
+  TabPanels,
+} from "@tremor/react";
+
+import "./styles.css";
+
+//import { BL } from "./components/BarList";
+
+function App() {
+  return (
+    <main className="p-12">
+      <Title>Dashboard</Title>
+      <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+
+      <TabGroup className="mt-6">
+        <TabList>
+          <Tab>Overview</Tab>
+          <Tab>Detail</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-2">
+              <Card>
+                <Text>Title</Text>
+                <Metric>KPI 1</Metric>
+              </Card>
+              <Card>
+                <Text>Title</Text>
+                <Metric>KPI 2</Metric>
+              </Card>
+              <Card>
+                <Text>Title</Text>
+                <Metric>KPI 3</Metric>
+              </Card>
+              <Col numColSpan={1} numColSpanLg={2}>
+                <Card></Card>
+              </Col>
+              <Card></Card>
+              <Col numColSpan={3}>
+                <Card>
+                  <TableApp />
+                </Card>
+              </Col>
+              <Card></Card>
+              <Card></Card>
+              <Card></Card>
+            </Grid>
+          </TabPanel>
+          <TabPanel>
+            <Col numColSpan={3}>
+              <Card>
+                <TableApp />
+              </Card>
+            </Col>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,106 @@
+import { StatusOnlineIcon, StatusOfflineIcon } from "@heroicons/react/outline";
+import {
+  Card,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Text,
+  Title,
+  Badge,
+} from "@tremor/react";
+
+const data = [
+  {
+    name: "Viola Amherd",
+    Role: "Federal Councillor",
+    departement:
+      "The Federal Department of Defence, Civil Protection and Sport (DDPS)",
+    status: "inactive",
+  },
+  {
+    name: "Simonetta Sommaruga",
+    Role: "Federal Councillor",
+    departement:
+      "The Federal Department of the Environment, Transport, Energy and Communications",
+    status: "active",
+  },
+  {
+    name: "Alain Berset",
+    Role: "Federal Councillor",
+    departement: "The Federal Department of Home Affairs (FDHA)",
+    status: "active",
+  },
+  {
+    name: "Ignazio Cassis",
+    Role: "Federal Councillor",
+    departement: "The Federal Department of Foreign Affairs (FDFA)",
+    status: "active",
+  },
+  {
+    name: "Ueli Maurer",
+    Role: "Federal Councillor",
+    departement: "The Federal Department of Finance (FDF)",
+    status: "active",
+  },
+  {
+    name: "Guy Parmelin",
+    Role: "Federal Councillor",
+    departement:
+      "The Federal Department of Economic Affairs, Education and Research (EAER)",
+    status: "active",
+  },
+  {
+    name: "Karin Keller-Sutter",
+    Role: "Federal Councillor",
+    departement: "The Federal Department of Justice and Police (FDJP)",
+    status: "inactive",
+  },
+];
+
+function TableApp() {
+  return (
+    <Card>
+      <Title>List of Swiss Federal Councillours</Title>
+      <Table className="mt-5">
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Name</TableHeaderCell>
+            <TableHeaderCell>Position</TableHeaderCell>
+            <TableHeaderCell>Department</TableHeaderCell>
+            <TableHeaderCell>Status</TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.name}>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>
+                <Text>{item.Role}</Text>
+              </TableCell>
+              <TableCell>
+                <Text>{item.departement}</Text>
+              </TableCell>
+              <TableCell>
+                <Badge
+                  color={item.status == "active" ? "emerald" : "rose"}
+                  icon={
+                    item.status == "active"
+                      ? StatusOnlineIcon
+                      : StatusOfflineIcon
+                  }
+                >
+                  {item.status}
+                </Badge>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  );
+}
+
+export default TableApp;
